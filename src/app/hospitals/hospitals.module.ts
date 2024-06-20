f29a3f711581e86030b4d6c4edb53a2df5e1932e
MarkuzLopez <marco.02lopez@hotmail.com>
2024-06-20 14:02:28 -0600

    feat: :sparkles: create struct of modules
    modules  doctors and hospitals

---
+++ src/app/hospitals/hospitals.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HospitalComponent } from './pages/hospital.component';
+
+
+
+@NgModule({
+  declarations: [
+    HospitalComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HospitalsModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BreadcumbsComponent } from './components/breadcumbs/breadcumbs.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BreadcumbsComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
   imports: [
     CommonModule
   ]
