9f583aacf0ab4a043f0fddd01dadbef1f1e767b2
MarkuzLopez <marco.02lopez@hotmail.com>
2024-06-21 12:53:07 -0600

    feat: :sparkles: validate email, password
    validate formregister email, validate

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -23,34 +23,53 @@ export class RegisterComponent {
   private initiForm(): void{
    this.registerForm = this.formBuilder.group({
       username: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
       confirmPassword: ['', Validators.required],
       terms: [false, Validators.required]
+    }, {
+      validators: this.passwordEquals('password', 'confirmPassword')
     })
   }
 
-
   onSubmit(): void {
-    this.formSubmited = true;
-    if( this.registerForm.valid && !this.acceptTerms()){
-      console.log(this.registerForm.value, 'formulario posteado..' );
-    }
+    this.formSubmited = true;    
+      if( this.registerForm.valid && !this.acceptTerms()){
+        console.log(this.registerForm.value, 'formulario posteado..' );
+        // this.registerForm.reset()
+      }    
+  }
+
+
+  // onSubmit(): void {
+  //   this.formSubmited = true;
+  //   if(this.validatePassword()) {
+  //     if( this.registerForm.valid && !this.acceptTerms()){
+  //       console.log(this.registerForm.value, 'formulario posteado..' );
+  //       this.registerForm.reset()
+  //     }
+  //   }
+  // }
+
+  validatePassword(): boolean { 
+    const password = this.registerForm.get('password')?.value;
+    const confirmPassword = this.registerForm.get('confirmPassword')?.value;
+    return (password !== confirmPassword && this.formSubmited) ? true : false;
   }
 
   validateForm = (): boolean => this.registerForm.invalid && this.formSubmited;
 
-  // validateForm(): boolean {    
-  //   if(this.registerForm.invalid && this.formSubmited) {
-  //     return true;
-  //   }
-  //   return false
-  // }
-
   acceptTerms = (): boolean => !this.registerForm.get('terms')?.value && this.formSubmited;
 
-  // acceptTerms(): boolean{    
-  //   return !this.registerForm.get('terms')?.value && this.formSubmited;
-  // }
-
+  passwordEquals(password: string, confirmPassword: string) {
+    return (formGroup: FormGroup) => {
+      const passwordControl = formGroup.get(password);
+      const confirmPasswordControl = formGroup.get(confirmPassword);
+      if(passwordControl?.value ===  confirmPasswordControl?.value) {
+        confirmPasswordControl?.setErrors(null);
+      } else { 
+        confirmPasswordControl?.setErrors({ notEquals: true });
+      }
+    }
+  }
 }
