0a44291d9e7352913ae276758b60a611be8d2d4d
MarkuzLopez <marco.02lopez@hotmail.com>
2024-06-24 13:38:16 -0600

    feat: :sparkles: add localStorage for token
    in create and login user.

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -2,25 +2,32 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Login } from '@auth/models/login';
 import { User } from '@auth/models/user';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private readonly baseUrl = environment.apiUrl;
 
-  private readonly baseUrl  = environment.apiUrl;
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { 
+  createUser(user: User): Observable<any> {
+    console.log(user, 'user');
+    return this.http.post(`${this.baseUrl}/usuarios/create`, user).pipe(
+      tap((res: any) => {
+        localStorage.setItem('token', res.token);
+      })
+    );
   }
 
-  createUser(user: User): Observable<any> {
-    console.log(user, 'user');    
-    return this.http.post(`${this.baseUrl}/usuarios/create`, user);
-  }
-
-  loginUser (user: Login): Observable<any> { 
-    return this.http.post(`${this.baseUrl}/auth/login`, user);
+  loginUser(user: Login): Observable<any> {
+    return this.http.post(`${this.baseUrl}/auth/login`, user).pipe(
+      tap((response: any) => {
+        localStorage.setItem('token', response?.token);
+      })
+    );
   }
 }
+// tap es otro operador observable que dispara otro efecto secundario.
