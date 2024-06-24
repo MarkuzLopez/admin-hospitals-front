0e03174e451df0194b1531a57cbc7af502ddd4b1
MarkuzLopez <marco.02lopez@hotmail.com>
2024-06-24 13:51:45 -0600

    feat: :sparkles: function for remember email
    save token for remember

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -22,8 +22,8 @@ export class LoginComponent implements OnInit{
 
 
   initiForm(): void { 
-    this.formLogin = this.formBuilder.group({      
-      email: ['', [Validators.required, Validators.email]],
+    this.formLogin = this.formBuilder.group({
+      email: [ localStorage.getItem('email') || null, [Validators.required, Validators.email]],
       password: ['', Validators.required],
       remember: [false ]      
     })
@@ -32,11 +32,13 @@ export class LoginComponent implements OnInit{
   onLogin() {
     this.submitted = true;
     if(this.formLogin.valid) {
+      // TODO  alerts for msg
       console.log('asda', this.formLogin.value);
       this.authService.loginUser(this.formLogin.value).subscribe((resonse) => { 
         console.log(resonse, 'tokens');
-        
+        this.rememberEmail();
       }, ({error}) => {
+        // TODO  alerts for msg
         console.error(error, 'errorr', error?.msg);
         
       })
@@ -44,18 +46,14 @@ export class LoginComponent implements OnInit{
     console.log('campos obligatorios'); 
   }
 
+  rememberEmail (): void { 
+    (this.formLogin.get('remember')?.value) ? 
+    localStorage.setItem('email', this.formLogin.get('email')?.value) : 
+    localStorage.removeItem('email');
+  }
+
   validateForm = (): boolean => this.formLogin.invalid && this.submitted;
 
-  // validFields (): boolean{ 
-  //   if( this.formLogin.invalid && this.submitted) {
-  //     return true
-  //   }
-  //   return false
-  // }
-  
-
-  signUp(): void {
-    this.router.navigateByUrl('/register');
-  }
+  signUp = () => this.router.navigateByUrl('/register');
 
 }
