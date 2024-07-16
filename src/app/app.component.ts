edcb5137ca37e45d3cdd2f156342487d7610a863
MarkuzLopez <marco.02lopez@hotmail.com>
2024-07-16 13:28:13 -0600

    feat: :sparkles: inactivity, closed session
    closed session by inactivity.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IdleService } from './services/idle.service';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = 'medical-admin-web';
+	constructor(private idService: IdleService) {}
+
+	ngOnInit(): void {
+		this.idService;
+	}
 }
---
+++ src/app/services/idle.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, NgZone } from '@angular/core';
+import { AuthService } from '@auth/service/auth.service';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class IdleService {
+	private idleTimeout: any;
+	private readonly idleTimeLimit = 300000; // cinco minutos
+
+	constructor(
+		private ngZone: NgZone,
+		private authService: AuthService
+	) {
+		this.resetIdleTimer();
+		this.setuEvenListieners();
+	}
+
+	private resetIdleTimer(): void {
+		clearTimeout(this.idleTimeout);
+		this.ngZone.runOutsideAngular(() => {
+			this.idleTimeout = setTimeout(() => this.logOut(), this.idleTimeLimit);
+		});
+	}
+
+	private setuEvenListieners(): void {
+		const events = ['mouse', 'mousedown', 'click', 'scroll', 'keypress'];
+		events.forEach((event) => {
+			document.addEventListener(event, () => this.resetIdleTimer(), true);
+		});
+	}
+
+	private logOut(): void {
+		this.authService.onLogout();
+	}
+}
